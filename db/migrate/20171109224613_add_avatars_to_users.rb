1279a3b7d73b4e968e3301c703a54a683c7b86e5
Patrick <pa@arthurfamily.ws>
2018-01-08 17:23:03 -0500

    fixed schema issues

--- db/migrate/20171109224613_add_avatars_to_users.rb
+++ db/migrate/20171109224613_add_avatars_to_users.rb
@@ -1,4 +1,4 @@
-class AddAvatarsToUsers < ActiveRecord::Migration
+class AddAvatarsToUsers < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :avatars, :json
   end
--- db/schema.rb
+++ db/schema.rb
@@ -38,24 +38,25 @@ ActiveRecord::Schema.define(version: 20171221192631) do
     t.index ["user_id"], name: "index_athlete_metrics_on_user_id"
   end
 
-  create_table "comments", id: :serial, force: :cascade do |t|
+  create_table "comments", force: :cascade do |t|
     t.string "title", limit: 50, default: ""
     t.text "comment"
-    t.integer "commentable_id"
     t.string "commentable_type"
-    t.integer "user_id"
+    t.bigint "commentable_id"
+    t.bigint "user_id"
     t.string "role", default: "comments"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["commentable_id"], name: "index_comments_on_commentable_id"
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
     t.index ["commentable_type"], name: "index_comments_on_commentable_type"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
-  create_table "posts", id: :serial, force: :cascade do |t|
+  create_table "posts", force: :cascade do |t|
     t.string "attachment"
     t.text "content"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
@@ -110,18 +111,20 @@ ActiveRecord::Schema.define(version: 20171221192631) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table "votes", id: :serial, force: :cascade do |t|
-    t.integer "votable_id"
+  create_table "votes", force: :cascade do |t|
     t.string "votable_type"
-    t.integer "voter_id"
+    t.bigint "votable_id"
     t.string "voter_type"
+    t.bigint "voter_id"
     t.boolean "vote_flag"
     t.string "vote_scope"
     t.integer "vote_weight"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope"
+    t.index ["votable_type", "votable_id"], name: "index_votes_on_votable_type_and_votable_id"
     t.index ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope"
+    t.index ["voter_type", "voter_id"], name: "index_votes_on_voter_type_and_voter_id"
   end
 
   add_foreign_key "posts", "users"
