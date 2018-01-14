ed2f715d13e16df49e5e90c1d6fce4a9325b5f12
Patrick <pa@arthurfamily.ws>
2018-01-14 18:39:34 -0500

    small refactor, added count

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -3,8 +3,7 @@ class ConversationsController < ApplicationController
 
   def create
     @conversation = Conversation.get(current_user.id, params[:user_id])
-    conversation_exists = conversated?
-    redirect_to conversation_path(@conversation.id) unless conversated?
+    redirect_to conversation_path(@conversation.id)
   end
 
   def show
@@ -14,24 +13,12 @@ class ConversationsController < ApplicationController
   def close
     @conversation = Conversation.find(params[:id])
 
-    session[:conversations].delete(@conversation.id)
     @conversation.destroy
 
     respond_to do |format|
       render :json => { :conversation => params[:id]}
       format.js
     end
-  end
-
-  private
-
-  def add_to_conversations
-    session[:conversations] ||= []
-    session[:conversations] << @conversation.id
-  end
-
-  def conversated?
-    session[:conversations].include?(@conversation.id)
   end
 end
 
